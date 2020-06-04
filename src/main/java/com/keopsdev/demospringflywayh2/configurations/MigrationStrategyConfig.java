a5f9e104d207cfcc51cba67ead3385d453fe5df6
KeroneJr <kerone@gmail.com>
2020-06-04 08:53:45 -0300

    Atualiação da classe MigrationStrategyConfig

--- src/main/java/com/keopsdev/demospringflywayh2/configurations/MigrationStrategyConfig.java
+++ src/main/java/com/keopsdev/demospringflywayh2/configurations/MigrationStrategyConfig.java
@@ -11,7 +11,15 @@ public class MigrationStrategyConfig {
     public FlywayMigrationStrategy flywayMigrationStrategy() {
         
     	return flyway -> {
+    		//PARA EVIRAR QUE O FLYWAY RODER BASTA COMENTAR A LINHA ABAIXO
             flyway.migrate();
         };
     }
+    
+    /* IMPLEMENTAÇÂO PRA FAZER O FLYWAY POPULAR O BANCO APOS O HIBERNATE CRIAR AS TABELA
+    @Autowired
+    public void FlywayConfiguration(DataSource dataSource) {
+        Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate(); //setschemas("").
+    }
+    */
 }
